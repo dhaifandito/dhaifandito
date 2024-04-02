828471102dd35b285e8dc4cc60d984bb1df00c43
dhaifandito <dhaifan.da@gamail.com>
2024-04-02 16:46:17 +0700

    add pdf

--- src/lib/components/Navbar.js
+++ src/lib/components/Navbar.js
@@ -11,12 +11,23 @@ function Navbar() {
     }
     const lightTheme = "bg-light-primary text-light-body"
     const darkTheme = "bg-dark-primary text-dark-body"
+
+    
+        const handleDownload = () => {
+          const pdfUrl = '/ditocv.pdf';
+      
+          const link = document.createElement('a');
+          link.href = pdfUrl;
+          link.download = 'dito-cv.pdf'; 
+      
+          link.click();
+        };
+
     return ( 
         <div className={`fixed z-10 h-12 w-full font-bold flex items-center justify-between font-roboto px-4 ${!isDark ? darkTheme : lightTheme}`}>
             <h1>Dito.</h1>
             <div className="flex items-center">
-            <Button className="mx-4" variant={isDark ? "dark" : "light"}>Get Dito CV</Button>
-            {/* <Button onClick={()=>{handleTheme()}} variant="outline">Change Theme</Button> */}
+            <Button className="mx-4" variant={isDark ? "dark" : "light"} onClick={()=>handleDownload()}>Get Dito CV</Button>
             <div onClick={()=>{handleTheme()}}>
             <Image
                         src={isDark ? "/sun-dark.svg" : "/sun-light.svg"}
